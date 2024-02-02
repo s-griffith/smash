4c12d9890819ca7318dcb11f6c01937aa6f1bc29
s-griffith <cmcgriffith@gmail.com>
2024-02-02 14:00:09 +0200

    fix

--- Commands.h
+++ Commands.h
@@ -149,7 +149,7 @@ class SmallShell {
   
   SmallShell(const std::string prompt = "smash");
  public:
-  static int m_pid;
+  static pid_t m_pid;
   Command *CreateCommand(const char* cmd_line);
   SmallShell(SmallShell const&)      = delete; // disable copy ctor
   void operator=(SmallShell const&)  = delete; // disable = operator
