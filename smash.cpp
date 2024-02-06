be96fdf54370d6ef8af717824f55c350e957f808
ShlomoAvn <shlomo.avnon@campus.technion.ac.il>
2024-02-06 14:54:22 +0200

    jobs- tests

--- smash.cpp
+++ smash.cpp
@@ -14,6 +14,7 @@ int main(int argc, char* argv[]) {
 
     SmallShell& smash = SmallShell::getInstance();
     while(true) {
+        std::cout<< "17" <<endl;
         std::cout << smash.getPrompt() << "> ";
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
