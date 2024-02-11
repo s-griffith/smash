e0a0bb4f999fe14d4b342273b41b8f3fdff328d2
ShlomoAvn <shlomo.avnon@campus.technion.ac.il>
2024-02-11 15:15:42 +0200

    quit- tests

--- Commands.cpp
+++ Commands.cpp
@@ -170,7 +170,7 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
   }
   else if (firstWord.compare("quit") == 0) {
    jobs.printJobsList();
-   //return new QuitCommand(cmd_line, &jobs);
+   return new QuitCommand(cmd_line, &jobs);
   }
 //others
   else {
@@ -319,7 +319,7 @@ void JobsList::removeFinishedJobs() {
   int numArgs=0;
       cout<<"315";
   char** args = (char**)malloc(COMMAND_MAX_ARGS * sizeof(char**) + 1);
-  args = getArgs(cmd_line, &numArgs);
+  //args = getArgs(cmd_line, &numArgs);
     //if(args[numArgs-1] == "kill"){
        SmallShell& smash = SmallShell::getInstance();
        //smash.getJobs()->killAllJobs();
