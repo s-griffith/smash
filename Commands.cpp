0e4f1560fd114bbe3f05728279fd973db3de4712
s-griffith <cmcgriffith@gmail.com>
2024-02-13 23:29:50 +0200

    valgrind

--- Commands.cpp
+++ Commands.cpp
@@ -856,7 +856,7 @@ void ChangeDirCommand::execute()
   if (string(args[1]) == "..")
   {
     smash.setPrevDir(smash.getCurrDir());
-    goUp(smash.getCurrDir())
+    goUp(smash.getCurrDir());
     return;
   }
   // If the new path is the full path, set currDir equal to it
